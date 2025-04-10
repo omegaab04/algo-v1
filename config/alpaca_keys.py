5e76c6a191987997f1641f87c605a37c303ba104
Adam B <adam.bouchenaf23@gmail.com>
2025-04-10 18:54:54 +0100

    key

---
+++ config/alpaca_keys.py
@@ -0,0 +1,7 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+API_KEY = os.getenv("ALPACA_API_KEY")
+API_SECRET = os.getenv("ALPACA_SECRET_KEY")
+BASE_URL = os.getenv("ALPACA_BASE_URL", "https://paper-api.alpaca.markets")
